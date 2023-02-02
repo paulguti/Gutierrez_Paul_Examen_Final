cb6a19749e8869322fe0ff61ca1efd958f8521f2
Paul Fernando Gutierrez <paulgut2001804@gmail.com>
2023-02-02 02:52:16 -0500

    Cambio 2

--- sketch.js
+++ sketch.js
@@ -1,16 +1,17 @@
-var puntos = []
+let puntos = []
+let mult = 0.005;
 
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   background(30,80,79)
 
-  var densidad = 50;
-  var espacio = width / densidad 
+  let densidad = 50;
+  let espacio = width / densidad 
 
-  for (var x = 0; x < width; x += espacio){
-    for (var y = 0; y < height; y += espacio) {
-      var p = createVector(x, y);
+  for (let x = 0; x < width; x += espacio){
+    for (let y = 0; y < height; y += espacio) {
+      let p = createVector(x, y);
       puntos.push(p);
     }
   }
@@ -21,7 +22,9 @@ function draw() {
   fill(225);
 
   for (var i = 0; i < puntos.length; i++) {
-    var angle = map(noise(puntos[i].x, puntos[i].y), 0, 1, 0, 720)
+    var angle = map(noise(puntos[i].x * mult, puntos[i].y * mult), 5, 1, 5, 720)
+
+    puntos [i].add(createVector(cos(angle), sin(angle)))
 
     ellipse(puntos[i].x, puntos[i].y, 1)
   }
